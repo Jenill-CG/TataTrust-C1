887a6cd77c32ca7fd6b362c08babca55368a9795
Jenill-CG <jenil@convegenius.ai>
2024-11-29 13:15:37 +0530

    Update Custom_1_2911.py

--- Custom_1_2911.py
+++ Custom_1_2911.py
@@ -99,9 +99,9 @@ def process_data(uploaded_file, partner_id, buffer_percent, grade, district_digi
     # Use the selected parameter set for generating Custom_ID
     data_expanded['Custom_ID'] = data_expanded.apply(lambda row: generate_custom_id(row, parameter_mapping[selected_param]), axis=1)
     # Generate the additional Excel sheets with mapped columns (without the Gender column)
-    data_mapped = data_expanded[['Custom_ID', 'Grade', 'School', 'School_ID', 'District', 'Block']].copy()
-    data_original_mapped = data_expanded[['Custom_ID', 'Grade', 'School', 'School_udise', 'District', 'Block']].copy()
-    data_mapped.columns = ['Roll_Number', 'Grade', 'School Name', 'School Code', 'District Name', 'Block Name','State_ID','Group_ID','SP_ID']
+    data_mapped = data_expanded[['Custom_ID', 'Grade', 'School', 'School_ID', 'District', 'Block','State_ID','Group_ID','SP_ID']].copy()
+    data_original_mapped = data_expanded[['Custom_ID', 'Grade', 'School', 'School_udise', 'District', 'Block','State_ID','Group_ID','SP_ID']].copy()
+    data_mapped.columns = ['Roll_Number', 'Grade', 'School Name', 'School Code', 'District Name', 'Block Name']
     data_original_mapped.columns = ['Roll_Number', 'Grade', 'School Name', 'School Code', 'District Name', 'Block Name']
     # Generate Teacher_Codes sheet
     teacher_codes = data[['School', 'School_ID']].copy()
