07d480881a799ee62a45c5f23c8fbce71a8b98f6
Jenill-CG <jenil@convegenius.ai>
2024-11-29 13:24:27 +0530

    Update Custom_1_2911.py

--- Custom_1_2911.py
+++ Custom_1_2911.py
@@ -74,9 +74,9 @@ def process_data(uploaded_file, partner_id, buffer_percent, grade, district_digi
     # Assign the Partner_ID directly
     data['Partner_ID'] = str(partner_id).zfill(len(str(partner_id)))  # Padding Partner_ID
     data['Grade'] = grade
-    data['State_ID'] = State_ID
-    data['Group_ID'] = Group_ID
-    data['SP_ID'] = SP_ID
+    data['State_ID'] = State
+    data['Group_ID'] = Group
+    data['SP_ID'] = SP
     # Assign unique IDs for District, Block, and School, default to "00" for missing values
     # data['School_udise'] = data['School_ID'].astype(str).str.zfill(12)
     data['School_udise'] = data['School_ID']
