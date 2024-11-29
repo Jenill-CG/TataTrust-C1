c023bfdea0694e32a5a3c0baa6857745b36988cd
Jenill-CG <jenil@convegenius.ai>
2024-11-29 12:44:35 +0530

    Update Custom_1_2911.py

--- Custom_1_2911.py
+++ Custom_1_2911.py
@@ -506,7 +506,7 @@ def main():
             # # Custom parameters
             # st.markdown("<p style='color: blue;'>Please provide required Values</p>", unsafe_allow_html=True)
             st.markdown("➡️ Please provide required Values", unsafe_allow_html=True)
-            col1, col2, col3 = st.columns([1,1,1,1,1,1])
+            col1, col2, col3,col4,col5,col6 = st.columns([1,1,1,1,1,1])
             with col1:
                 partner_id = st.number_input("Partner ID", min_value=1, value=1)
             with col2:
