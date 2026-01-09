5c9af17f62f1b1c3fc8ed279821cdaf0bb1613d4
Jenill-CG <jenil@convegenius.ai>
2026-01-09 19:13:44 +0530

    Update Custom_1_2012.py

--- Custom_1_2012.py
+++ Custom_1_2012.py
@@ -218,7 +218,8 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
     # Reset the font back to the original for the remaining labels
     pdf.set_font('Arial', 'B', 5)
     
-    minusone = info_labels['CLASS']+1
+    # minusone = info_labels['CLASS']+1
+    minusone = info_labels['CLASS']+2
     
     # Add the CLASS and SECTION labels
     pdf.cell(info_cell_width, 3, f"CLASS : {minusone}", border='LR', ln=1)
